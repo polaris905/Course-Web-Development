e2ba84f4e38c24f40d8820ba4a4485279cbff141
POLARIS <37786972+polaris905@users.noreply.github.com>
2018-04-05 14:39:43 +0800

    Color Toggle Exercise

---
+++ colorToggleExercise.js
@@ -0,0 +1,17 @@
+var button = document.querySelector("button");
+/*我之前用的是这种算法，实际上用下面那种更加简洁
+  var isPurple = false;
+  button.addEventListener("click", function(){
+   	if(isPurple){
+  		document.body.style.background = "white";
+  	} 
+    else {
+  		document.body.style.background = "purple";
+  	}
+  	isPurple = !isPurple;
+  });
+*/
+
+button.addEventListener("click", function(){
+	document.body.classList.toggle("purple");
+});
